8a6e616bdfd3a9e8decb86cc42fa5bec970087da
calexander3108 <127106980+calexander3108@users.noreply.github.com>
2023-03-07 06:58:11 -0800

    bug fix hover

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -19,20 +19,20 @@ const Sidebar = () => {
           <img src="/logo.png" className="block w-[22px] -pl-2 pt-1 lg:pt-0" />
           <span className="pl-[1.5px] hidden lg:inline">Clerkie Challenge</span>
         </a>
-        <a
+        <Link
           href="/"
-          className="flex items-center space-x-1 rounded-md px-0 py-2 lg:px-2 lg:py-3"
+          className="flex items-center space-x-1 rounded-md px-0 py-2 lg:px-2 lg:py-3 hover:bg-[#424242]"
         >
           <span className="text-2xl">
             <i className="bx bx-home"></i>
           </span>
           <img src="/homeIcon.png" className="inline w-[24px] -pl-2" />
           <span className="pl-[1.5px] hidden lg:inline">Home</span>
-        </a>
+        </Link>
 
         <Link
           href="/friends"
-          className="flex items-center space-x-1 rounded-md px-0 py-2 lg:px-2 lg:py-3 bg-[#424242]"
+          className="flex items-center space-x-1 rounded-md px-0 py-2 lg:px-2 lg:py-3 hover:bg-[#424242]"
         >
           <span className="text-2xl">
             <i className="bx bx-cart"></i>
